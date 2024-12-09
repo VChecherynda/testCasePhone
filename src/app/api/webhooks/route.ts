3a77e2b8abd5b1fbbc4d55dfc589230ef667dd80
vchecherynda <checherinda.vadym@gmail.com>
2024-12-09 23:15:07 +0200

    Update header

--- src/app/api/webhooks/route.ts
+++ src/app/api/webhooks/route.ts
@@ -1,6 +1,5 @@
 import { db } from "@/db";
 import { stripe } from "@/lib/stripe";
-import { headers } from "next/headers";
 import { NextResponse } from "next/server";
 import Stripe from "stripe";
 
