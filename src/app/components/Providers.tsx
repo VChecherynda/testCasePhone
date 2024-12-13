8972606dee05b41f69fd20e3c8d3fcc6d2deccf1
vchecherynda <checherinda.vadym@gmail.com>
2024-12-13 18:49:23 +0200

    Add Kinde Provider

--- src/app/components/Providers.tsx
+++ src/app/components/Providers.tsx
@@ -2,11 +2,16 @@
 
 import { ReactNode } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { KindeProvider } from "@kinde-oss/kinde-auth-nextjs";
 
 const client = new QueryClient();
 
 const Providers = ({ children }: { children: ReactNode }) => {
-  return <QueryClientProvider client={client}>{children}</QueryClientProvider>;
+  return (
+    <KindeProvider>
+      <QueryClientProvider client={client}>{children}</QueryClientProvider>
+    </KindeProvider>
+  );
 };
 
 export default Providers;
