70aa4f0920288baaaf7d71914570533e6fb9ad2a
vchecherynda <checherinda.vadym@gmail.com>
2024-12-13 18:41:13 +0200

    Resolve error

--- src/app/auth-callback/page.tsx
+++ src/app/auth-callback/page.tsx
@@ -24,14 +24,16 @@ const Page = () => {
     retryDelay: 500,
   });
 
-  if (data?.success) {
-    if (configId) {
-      localStorage.removeItem("configurationId");
-      router.push(`/configure/preview?id=${configId}`);
-    } else {
-      router.push("/");
+  useEffect(() => {
+    if (data?.success) {
+      if (configId) {
+        localStorage.removeItem("configurationId");
+        router.push(`/configure/preview?id=${configId}`);
+      } else {
+        router.push("/");
+      }
     }
-  }
+  }, [data?.success, configId, router]);
 
   return (
     <div className="mt-24 flex w-full justify-center">
